8463d4fc6b5839c28d0ac687cc733c6570daf840
Erla-B <111748576+Erla-B@users.noreply.github.com>
2023-05-10 19:50:24 +0000

    Fixing errors

--- app/posts/{postId}/page.tsx
+++ app/posts/{postId}/page.tsx
@@ -1,56 +1,59 @@
-import React from "react";
-import getFormattedDate from "@/lib/getFormattedDate";
-import { getSortedPostsData, getPostData } from "@/lib/posts";
-import { notFound } from "next/navigation";
-import Link from "next/link";
+import getFormattedDate from "@/lib/getFormattedDate"
+import { getSortedPostsData, getPostData } from "@/lib/posts"
+import { notFound } from "next/navigation"
+import Link from "next/link"
 
 export function generateStaticParams() {
-  const posts = getSortedPostsData(); //deduped!
+    const posts = getSortedPostsData() //deduped!
 
-  return posts.map((post) => ({
-    postId: post.id,
-  }));
+    return posts.map((post) => ({
+        postId: post.id
+    }))
 }
 
 export function generateMetadata({ params }: { params: { postId: string } }) {
-  const posts = getSortedPostsData(); //deduped!
-  const { postId } = params;
 
-  const post = posts.find((post) => post.id === postId);
+    const posts = getSortedPostsData() //deduped!
+    const { postId } = params
 
-  if (!post) {
+    const post = posts.find(post => post.id === postId)
+
+    if (!post) {
+        return {
+            title: 'Post Not Found'
+        }
+    }
+
     return {
-      title: "Post Not Found",
-    };
-  }
-
-  return {
-    title: post.title,
-  };
+        title: post.title,
+    }
 }
 
 export default async function Post({ params }: { params: { postId: string } }) {
-  const posts = getSortedPostsData(); //deduped!
-  const { postId } = params;
 
-  if (!posts.find((post) => post.id === postId)) {
-    return notFound();
-  }
+    const posts = getSortedPostsData() //deduped!
+    const { postId } = params
 
-  const { title, date, contentHtml } = await getPostData(postId);
+    if (!posts.find(post => post.id === postId)) {
+        return notFound()
+    }
 
-  const pubDate = getFormattedDate(date);
+    const { title, date, contentHtml } = await getPostData(postId)
 
-  return (
-    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
-      <p className="mt-0">{pubDate}</p>
-      <article>
-        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-        <p>
-          <Link href="/">← Back to home</Link>
-        </p>
-      </article>
-    </main>
-  );
+    const pubDate = getFormattedDate(date)
+
+    return (
+        <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+            <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+            <p className="mt-0">
+                {pubDate}
+            </p>
+            <article>
+                <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+                <p>
+                    <Link href="/">← Back to home</Link>
+                </p>
+            </article>
+        </main>
+    )
 }
